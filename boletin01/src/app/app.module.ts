bbc034e695639b4cbae9b4253a3271dc0c797649
Durbanban <carlosdurban91@gmail.com>
2022-09-29 19:50:33 +0200

    Planteado el ejercicio 1

---
+++ boletin01/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { Ej01LoginComponent } from './components/ej01-login/ej01-login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Ej01LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ boletin01/src/app/components/ej01-login/ej01-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ej01-login',
+  templateUrl: './ej01-login.component.html',
+  styleUrls: ['./ej01-login.component.css']
+})
+export class Ej01LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
