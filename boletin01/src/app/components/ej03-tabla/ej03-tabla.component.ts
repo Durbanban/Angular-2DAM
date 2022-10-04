cff33151471010dab134cf2b7b11f0288663bd5c
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-10-04 14:28:05 +0200

    Hay que terminar el ejercicio 3

--- boletin01/src/app/components/ej03-tabla/ej03-tabla.component.ts
+++ boletin01/src/app/components/ej03-tabla/ej03-tabla.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+
+export interface Alumno {
+  nombre: string;
+  apellidos: string;
+  fechaNacimiento: string;
+  curso: string;
+}
+
+const ALUMNOS: Alumno[] = [
+  {nombre: 'Mario', apellidos: 'Ruiz', fechaNacimiento: '20-05-1993', curso: '2ºDAM'},
+  {nombre: 'Carlos Jesús', apellidos: 'Durbán Viloca', fechaNacimiento: '20-05-2002', curso: '2ºDAM'},
+  {nombre: 'Valentín', apellidos: 'Tola Rodríguez', fechaNacimiento: '07-08-1999', curso: '2ºDAM'},
+  {nombre: 'Arturo', apellidos: 'Céspedes', fechaNacimiento: '06-12-2001', curso: '2ºDAM'},
+  {nombre: 'Jose Ignacio', apellidos: 'Rivas', fechaNacimiento: '12-10-1995', curso: '2ºDAM'},
+ 
+];
 
 @Component({
   selector: 'app-ej03-tabla',
@@ -7,9 +24,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ej03TablaComponent implements OnInit {
 
+  
+  columnas: string[] = ['name', 'surname', 'birthdate', 'course', 'acciones'];
+  lista=ALUMNOS;
+  
+  @ViewChild(MatTable)
+  table!: MatTable<Alumno>;
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
+
+  borrarColumnas() {
+    /*EN CONSTRUCCIÓN*/
+  }
+
+  borrarAlumno(alumno: Alumno) {
+    this.lista.splice(this.lista.indexOf(alumno), 1);
+    this.table.renderRows();
+
+    
+    /*
+    let listaAux = this.lista;
+    let borrado=listaAux.indexOf(alumno);
+    listaAux.splice(borrado, 1);
+    this.lista=listaAux;
+    this.table.renderRows();
+    */
+  }
+
+  editarAlumno(alumno: Alumno) {
+    /*EN CONSTRUCCIÓN*/
+
+    
+  }
+
+
+
 
 }
