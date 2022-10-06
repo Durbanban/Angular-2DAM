3e688c83a5f9b065a846c203952271f3696a74ea
Durbanban <carlosdurban91@gmail.com>
2022-10-06 23:22:16 +0200

    .

--- StarWars2/src/app/services/film.service.ts
+++ StarWars2/src/app/services/film.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CharacterResponse } from '../interfaces/character.interface';
 
 
 
@@ -11,4 +13,8 @@ const API_BASE_URL = 'https://swapi.dev/api';
 export class FilmService {
 
   constructor(private http: HttpClient) { }
+
+  public getFilms(): Observable<CharacterResponse> {
+    return this.http.get<CharacterResponse>(`${API_BASE_URL}/films/`);
+  }
 }
