0fd76d02ab07ec9a3b9c398ad3c9e1ea9861fe36
Durbanban <carlosdurban91@gmail.com>
2022-10-11 19:03:23 +0200

    Enrutación prácticamente terminada

--- starwarsCompleto/src/app/app-routing.module.ts
+++ starwarsCompleto/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './components/character/character.component';
+import { FilmComponent } from './components/film/film.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PlanetComponent } from './components/planet/planet.component';
+import { SpeciesComponent } from './components/species/species.component';
+import { StarshipComponent } from './components/starship/starship.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing', component: LandingComponent},
+  {path: 'characters', component: CharacterComponent},
+  {path: 'films', component: FilmComponent},
+  {path: 'planets', component: PlanetComponent},
+  {path: 'species', component: SpeciesComponent},
+  {path: 'starships', component: StarshipComponent},
+  {path: 'vehicles', component: VehicleComponent},
+  {path: '', redirectTo: '/landing', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- starwarsCompleto/src/app/app.module.ts
+++ starwarsCompleto/src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CharacterComponent } from './components/character/character.component';
+import { PlanetComponent } from './components/planet/planet.component';
+import { SpeciesComponent } from './components/species/species.component';
+import { FilmComponent } from './components/film/film.component';
+import { StarshipComponent } from './components/starship/starship.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    PlanetComponent,
+    SpeciesComponent,
+    FilmComponent,
+    StarshipComponent,
+    VehicleComponent,
+    LandingComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialImportsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
