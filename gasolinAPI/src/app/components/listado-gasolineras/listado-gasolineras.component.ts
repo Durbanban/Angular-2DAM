826640da55f477676756167d6817a0c52fd38262
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-11-14 14:31:57 +0100

    Rematamos en casa

--- gasolinAPI/src/app/components/listado-gasolineras/listado-gasolineras.component.ts
+++ gasolinAPI/src/app/components/listado-gasolineras/listado-gasolineras.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Gasolinera } from 'src/app/interfaces/gasolinera.interface';
+
 import { Municipio } from 'src/app/interfaces/municipio.interface';
 import { Provincia } from 'src/app/interfaces/provincia.interface';
 
@@ -25,58 +26,50 @@ export class ListadoGasolinerasComponent implements OnInit {
   fuelSelected = 'Precio Gasolina 95 E5';
   precio: number = 5;
   gasListFiltered: Gasolinera[] = [];
+  gasListFilteredByProvince: Gasolinera[] = []
+  gasListFilteredByProvinceAndMunicipio: Gasolinera[] = [];
   provinceList: Provincia[] = [];
   municipioList: Municipio[] = [];
   provinceSelected = '';
   fuelAttr: Gasolinera = {} as Gasolinera;
   orden = '';
   checkOrder = false;
-  myControl = new FormControl('');
-  options: Municipio[] = [];
-  filteredOptions: Observable<string> | undefined;
+  municipioSelected = '';
+  fuel: keyof typeof this.fuelAttr = 'Precio Gasolina 95 E5';
   
 
   ngOnInit(): void {
     
-    
+    this.getLocation();
     this.gasolineraService.getListadoGasolineras().subscribe((respuesta) => {
       this.gasList = respuesta.ListaEESSPrecio;
       this.gasListFiltered = respuesta.ListaEESSPrecio;
+      this.fuelAttr = this.gasList[0];
       this.provinciaService.getProvincias().subscribe(respuesta => {
         this.provinceList = respuesta;
-
-      })
+      });
     });
   }
+
   formatLabel(value: number) {
     return value;
   }
   
   priceFilter() {
-    this.filterBack();
-    this.gasListFiltered = this.gasListFiltered.filter((gasolinera) => this.applyFilter(this.toNumber(gasolinera['Precio Gasolina 95 E5'])) && gasolinera.Provincia == this.provinceSelected);
-    
+    if(this.provinceSelected != '' && this.municipioSelected != ''){
+      this.gasListFiltered = this.gasList.filter((gasolinera) => this.toNumber(gasolinera[this.fuel]) != 0 && this.toNumber(gasolinera[this.fuel]) <= this.precio && gasolinera.IDMunicipio == this.municipioSelected && gasolinera.IDProvincia == this.provinceSelected);
+    }else if(this.provinceSelected != '' && this.municipioSelected == ''){
+      this.gasListFiltered = this.gasList.filter((gasolinera) => this.toNumber(gasolinera[this.fuel]) != 0 && this.toNumber(gasolinera[this.fuel]) <= this.precio && gasolinera.IDProvincia == this.provinceSelected);
+    }else if(this.provinceSelected == '' && this.municipioSelected == '') {
+      this.gasListFiltered = this.gasList.filter((gasolinera) => this.toNumber(gasolinera[this.fuel]) != 0 && this.toNumber(gasolinera[this.fuel]) <= this.precio);
+    }    
   }
 
   toNumber(cadena: string) {
     return Number(cadena.replace(',', '.'));
   }
 
-  applyFilter(filtro: number): boolean {
-    if(filtro != 0) {
-      if(filtro <= this.precio) {
-        return true;
-      }else {
-        return false;
-      }
-    }else {
-      if(this.provinceSelected != '') {
-        return true;
-      }else {
-        return false;
-      }
-    }
-  }
+  
 
   sorting() {
     if(this.checkOrder) {
@@ -88,9 +81,9 @@ export class ListadoGasolinerasComponent implements OnInit {
 
   sortByMinPrice() {
     this.gasListFiltered = this.gasListFiltered.sort((gasStA, gasStB) => {
-      if(this.toNumber(gasStA['Precio Gasolina 95 E5']) > this.toNumber(gasStB['Precio Gasolina 95 E5'])) {
+      if(this.toNumber(gasStA[this.fuel]) > this.toNumber(gasStB[this.fuel])) {
         return 1;
-      }else if (this.toNumber(gasStA['Precio Gasolina 95 E5']) < this.toNumber(gasStB['Precio Gasolina 95 E5'])) {
+      }else if (this.toNumber(gasStA[this.fuel]) < this.toNumber(gasStB[this.fuel])) {
         return -1;
       }else {
         return 0;
@@ -103,9 +96,9 @@ export class ListadoGasolinerasComponent implements OnInit {
 
   sortByMaxPrice() {
     this.gasListFiltered = this.gasListFiltered.sort((gasStA, gasStB) => {
-      if(this.toNumber(gasStA['Precio Gasolina 95 E5']) < this.toNumber(gasStB['Precio Gasolina 95 E5'])) {
+      if(this.toNumber(gasStA[this.fuel]) < this.toNumber(gasStB[this.fuel])) {
         return 1;
-      }else if (this.toNumber(gasStA['Precio Gasolina 95 E5']) > this.toNumber(gasStB['Precio Gasolina 95 E5'])) {
+      }else if (this.toNumber(gasStA[this.fuel]) > this.toNumber(gasStB[this.fuel])) {
         return -1;
       }else {
         return 0;
@@ -116,23 +109,47 @@ export class ListadoGasolinerasComponent implements OnInit {
     this.checkOrder = !this.checkOrder;
   }
 
-  selectFuel(fuelType: keyof typeof this.fuelAttr) {
-    this.fuelSelected = fuelType;
-  }
-
-  filterBack() {
-    this.gasListFiltered = this.gasList;
-  }
+  
 
   provinceFilter() {
-    this.filterBack();
-    this.gasListFiltered = this.gasListFiltered.filter((gasolinera) => gasolinera.Provincia == this.provinceSelected && this.applyFilter(this.toNumber(gasolinera['Precio Gasolina 95 E5'])));
+   this.municipioSelected = '';
+   this.municipioService.getMunicipiosByIdProvincia(this.provinceSelected).subscribe(respuesta => {
+    this.municipioList = respuesta;
+   });
+   this.priceFilter();
   }
 
-  getMunicipios(id: string) {
-    this.municipioService.getMunicipiosByIdProvincia(id).subscribe(respuesta => {
-      this.municipioList = respuesta;
-    })
+  municipioFilter() {
+    this.gasListFilteredByProvinceAndMunicipio = this.gasListFilteredByProvince;
+    this.gasListFilteredByProvinceAndMunicipio = this.gasListFilteredByProvince.filter((gasolinera) => gasolinera.IDMunicipio == this.municipioSelected);
+  }
+
+  clearFilter() {
+    this.provinceSelected = '';
+    this.municipioSelected = '';
+    this.priceFilter();
+  }
+
+  clearFilterMunicipio() {
+    this.municipioSelected = '';
+    this.priceFilter();
+  }
+
+  getLocation(): void{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          const longitude = position.coords.longitude;
+          const latitude = position.coords.latitude;
+          this.callApi(longitude, latitude);
+        });
+    } else {
+       console.log("No support for geolocation")
+    }
+  }
+
+  callApi(Longitude: number, Latitude: number){
+    const url = `https://api-adresse.data.gouv.fr/reverse/?lon=${Longitude}&lat=${Latitude}`
+    //Call API
   }
 
 
--- gasolinAPI/src/app/components/una-gasolinera/una-gasolinera.component.ts
+++ gasolinAPI/src/app/components/una-gasolinera/una-gasolinera.component.ts
@@ -2,9 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Gasolinera } from 'src/app/interfaces/gasolinera.interface';
 
-
-import { GasolineraService } from 'src/app/services/gasolinera.service';
-
 @Component({
   selector: 'app-una-gasolinera',
   templateUrl: './una-gasolinera.component.html',
